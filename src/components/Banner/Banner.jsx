de9e72110a54bf4b6174dc99392454791cfe555b
selimRU <selim.soha92@gmail.com>
2023-09-27 15:11:44 +0600

    input field updated for responsivness

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -11,10 +11,10 @@ const Banner = ({ categories, foundData, handleSearch, handleinput, error, query
                     <img className="h-[400px] md:h-full md:w-full" src={bg} alt="" />
                 </div>
                 <div className=" bg-[#FFFFFFF2] w-full h-full absolute bottom-0"></div>
-                <div className=" absolute bottom-[30%] md:bottom-[40%] lg:bottom-[35%] md:left-[25%] left-[10%]  lg:left-[30%]">
+                <div className=" absolute bottom-[30%] md:bottom-[40%] lg:bottom-[35%] md:left-[25%] left-[9%]  lg:left-[30%]">
                     <h2 className=' md:text-2xl lg:text-4xl font-bold text-center text-[#0B0B0B] pb-10 placeholder:text-xs'>I Grow By Helping People In Need</h2>
                     <div className=' flex items-center justify-center'>
-                        <input onChange={handleinput} className=' border py-[5px] rounded-l-md px-5' type="text" placeholder='Search here' value={query} />
+                        <input onChange={handleinput} className=' border py-[5px] rounded-l-md  lg:px-5' type="text" placeholder='Search here' value={query} />
                         <button onClick={handleSearch} className=' bg-[#FF444A] text-sm text-[#FFF] font-semibold px-5 py-[8px] rounded-r-md'>Search</button>
                     </div>
                     <p className=" text-red-500 text-xs py-3 pl-[2px] lg:pl-[115px] md:pl-[18px]">{error}</p>
